5e55f7f7d6fc792971bc56e92635c9a968e49919
unknown <msushil@DWS-252N72S.dws.com.au>
2013-05-08 11:42:41 +1000

    initial commit

---
+++ webinvoice/WebInvoice/src/com/glassbox/webinvoice/client/WebInvoice.java
@@ -0,0 +1,23 @@
+package com.glassbox.webinvoice.client;
+import com.glassbox.webinvoice.client.ui.Main;
+import com.glassbox.webinvoice.client.ui.container.pages.LoginBox;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.Window;
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class WebInvoice implements EntryPoint {
+	private Main main = new Main();
+	private LoginBox login = new LoginBox();
+	
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+	    Window.enableScrolling(true);
+	    Window.setMargin("0px");
+	    RootPanel.get().add(main);
+	    login.show();
+	}
+}
