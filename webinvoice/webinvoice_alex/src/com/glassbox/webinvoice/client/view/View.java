c384714eeae2b6894f491319800bdfea93795262
kloseking7 <wayf128@hotmail.com>
2013-05-10 00:07:14 +1000

    remove defact one

--- webinvoice/webinvoice_alex/src/com/glassbox/webinvoice/client/view/View.java
+++
@@ -1,7 +0,0 @@
-package com.glassbox.webinvoice.client.view;
-
-import com.google.gwt.user.client.ui.Widget;
-
-public interface View {
-	Widget load();
-}
--- webinvoice/webinvoice_alex/src/com/glassbox/webinvoice/shared/entity/RoleType.java
+++
@@ -1,8 +0,0 @@
-package com.glassbox.webinvoice.shared.entity;
-
-import com.google.gwt.user.client.rpc.IsSerializable;
-
-public enum RoleType implements IsSerializable {
-	USER, CLIENT;
-
-}
