2a95953c6b8aae1987d953b0b358ec1631970895
manssster <manssster@gmail.com>
2013-06-12 22:30:08 +1000

    commit

---
+++ webinvoice/WebInvoice/src/main/java/com/glassbox/webinvoice/client/service/ClientServiceAsync.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.glassbox.webinvoice.client.service;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ *
+ * @author mon2
+ */
+public interface ClientServiceAsync {
+        public void getClients(AsyncCallback callback);
+}
